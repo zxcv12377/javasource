ce82363f23add47d287614d1977e31fb1501512f
ShimHyunJin <zxcv12377@naver.com>
2025-03-04 17:35:48 +0900

    클래스 예제

--- ch6_oop/src/oop/ProductEx1.java
+++ ch6_oop/src/oop/ProductEx1.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class ProductEx1 {
     public static void main(String[] args) {
-        Product product1 = new Product("p10214", "운동화", 124000, null);
+        Product product1 = new Product();
 
         product1.pCode = "p10214";
         product1.pName = "운동화";
