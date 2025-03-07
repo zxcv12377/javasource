59963b774b2d3223992fe0ff34b6ada5ec3bf518
ShimHyunJin <zxcv12377@naver.com>
2025-03-07 18:09:52 +0900

    피보나치 수열 테스트

---
+++ doitjava_test/src/solocoding/FibonacciTest.java
@@ -0,0 +1,20 @@
+package solocoding;
+
+public class FibonacciTest {
+    public static Long f(int a) {
+        Long[] arr = new Long[a + 1];
+        arr[0] = 0L;
+        arr[1] = 1L;
+        // arr[2] = 1
+        // arr[3] =
+        for (int i = 2; i <= a; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[a];
+    }
+
+    public static void main(String[] args) {
+        int a = 80;
+        System.out.println(f(a));
+    }
+}
