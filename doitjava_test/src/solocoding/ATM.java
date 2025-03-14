90ae9ce908b3d3129f8f8b82ffd40e3eae665194
ShimHyunJin <zxcv12377@naver.com>
2025-03-14 09:38:31 +0900

    a

--- doitjava_test/src/solocoding/ATM.java
+++ doitjava_test/src/solocoding/ATM.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ATM {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = Integer.parseInt(sc.nextLine());
         int[] arr = new int[n];
         String input = sc.nextLine();
